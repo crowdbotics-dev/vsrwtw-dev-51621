cb67db0bd20cb7dca5d4d9c49c9dee24814ec769
crowdbotics-dev <engineering@crowdbotics.com>
2022-12-21 19:58:00 +0000

    Update models

--- backend/home/admin.py
+++ backend/home/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import NweStudio
+admin.site.register(NweStudio)
 
 # Register your models here.
--- backend/home/models.py
+++ backend/home/models.py
@@ -1,1 +1,5 @@
+from django.conf import settings
 from django.db import models
+class NweStudio(models.Model):
+    'Generated Model'
+    sprint30 = models.BigIntegerField()
